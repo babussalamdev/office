c9a7a390672cbf830534f9309251cd4d2365a490
fauzan <fauzan101005gunawan@gmail.com>
2024-06-12 15:49:00 +0700

    update 12 Juni

--- store/home/mutations.js
+++ store/home/mutations.js
@@ -6,13 +6,13 @@ export default {
       { status: "Pegawai Inaktif", jumlah: value.inactive },
     ];
 
-    const sample = pegawaiData.map((item, index) => {
-      const percentage = (item.jumlah / value.total) * 100;
-      console.log(percentage)
-      return {
-        series: [percentage.toFixed(0)],
+    let sample
+
+    if (value.total === 0) {
+      sample = pegawaiData.map(item => ({
+        series: [0],
         chartOptions: {
-          colors: [state.colors[index]],
+          colors: [],
           chart: {
             height: 140,
             type: "radialBar",
@@ -45,8 +45,50 @@ export default {
           },
         },
         labels: [item.status],
-      };
-    });
+      }));
+    } else {
+      sample = pegawaiData.map((item, index) => {
+        const percentage = (item.jumlah / value.total) * 100;
+        return {
+          series: [percentage.toFixed(0)],
+          chartOptions: {
+            colors: [state.colors[index]],
+            chart: {
+              height: 140,
+              type: "radialBar",
+            },
+            plotOptions: {
+              radialBar: {
+                hollow: {
+                  margin: 2,
+                  size: "40%",
+                },
+                itemLabels: {
+                  style: {
+                    colors: ["#F44336", "#E91E63", "#9C27B0"],
+                  },
+                  showOn: "always",
+                  name: {
+                    offsetY: -20,
+                    show: false,
+                    color: "#000",
+                    fontSize: "13px",
+                  },
+                  value: {
+                    offsetY: 4,
+                    color: "#000",
+                    fontSize: "14px",
+                    show: true,
+                  },
+                },
+              },
+            },
+          },
+          labels: [item.status],
+        };
+      });
+    }
+
     state.pegawaiData = pegawaiData
     state.chartOptions = sample;
     state.pegawaiData.forEach((item, index) => {
