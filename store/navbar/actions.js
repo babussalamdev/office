da5973d98b60a64834ccab3131b797746b739ce1
SalityLook <hariyadiabuharits@gmail.com>
2024-09-16 19:02:42 +0700

    fix includes permission

--- store/navbar/actions.js
+++ store/navbar/actions.js
@@ -1,28 +1,29 @@
 export default {
-  setUnit({ commit, state, dispatch, rootState }, datas) {
+  async setUnit({ commit, state, dispatch, rootState }, datas) {
     localStorage.setItem("program", state.unit);
     if (state.unit === "root") {
       commit("index/root", null, { root: true });
     } else {
-      dispatch("index/changeUnit", state.unit, { root: true });
+      await dispatch("index/changeUnit", state.unit, { root: true });
     }
+    const permissions = rootState.index.permissions[0] !== 'none' ? rootState.index.permissions : [];
     const name = datas.route
     if (name === "index") {
       dispatch(`home/setMainChart`, state.unit, { root: true });
     } else if (name === "settings-mapel") {
-      if (rootState.index.permissions.includes('setup mapel')) {
+      if (permissions.includes('setup mapel')) {
         dispatch(`mapel/changeUnit`, state.unit, { root: true });
       } else {
         this.$router.push('/')
       }
     } else if (name === "settings-asrama") {
-      if (rootState.index.permissions.includes('setup asrama')) {
+      if (permissions.includes('setup asrama')) {
         dispatch(`kelompok/changeUnitAsrama`, state.unit, { root: true });
       } else {
         this.$router.push('/')
       }
     } else if (name === "settings-kelas") {
-      if (rootState.index.permissions.includes('setup kelas')) {
+      if (permissions.includes('setup kelas')) {
         dispatch(`kelas/changeUnit`, state.unit, { root: true });
       } else {
         this.$router.push('/')
@@ -36,19 +37,19 @@ export default {
     } else if (name === "settings-setupabsensi") {
       dispatch(`setupabsensi/getAbsensi`, state.unit, { root: true });
     } else if (name === "settings-nilaiquran") {
-      if (rootState.index.permissions.includes('setup nilai quran')) {
+      if (permissions.includes('setup nilai quran')) {
         dispatch(`setupnilaiquran/changeUnit`, null, { root: true });
       } else {
         this.$router.push('/')
       }
     } else if (name === "settings-halaqah") {
-      if (rootState.index.permissions.includes('setup halaqah')) {
+      if (permissions.includes('setup halaqah')) {
         dispatch(`kelompok/changeUnitHalaqah`, state.unit, { root: true });
       } else {
         this.$router.push('/')
       }
     } else if (name === "santri-database") {
-      if (rootState.index.permissions.includes('data santri')) {
+      if (permissions.includes('data santri')) {
         dispatch(`santri/database/changeUnit`, state.unit, { root: true });
       } else {
         this.$router.push('/')
@@ -58,7 +59,7 @@ export default {
     } else if (name === 'settings-kelas') {
       dispatch('kelas/changeUnit', state.unit, { root: true })
     } else if (name === 'settings-absensi') {
-      if (rootState.index.permissions.includes('setup absensi')) {
+      if (permissions.includes('setup absensi')) {
         dispatch('setupabsensi/getAbsensi', state.unit, { root: true })
       } else {
         this.$router.push('/')
@@ -66,7 +67,7 @@ export default {
     } else if (name === 'settings-struktur') {
       dispatch('struktur/changeUnit', state.unit, { root: true })
     } else if (name === 'pengampu') {
-      if (rootState.index.permissions.includes('absensi pengampu')) {
+      if (permissions.includes('absensi pengampu')) {
         dispatch('pengampu/changeUnit', null, { root: true })
       } else {
         this.$router.push('/')
@@ -88,7 +89,7 @@ export default {
     } else if (name === 'pelanggaran') {
       dispatch('pelanggaran/changeUnit', null, { root: true })
     } else if (name === 'report-mapel') {
-      if (rootState.index.permissions.includes('report mapel')) {
+      if (permissions.includes('report mapel')) {
         dispatch('report/nilaimapel/changeUnit', null, { root: true })
       } else {
         this.$router.push('/')
@@ -100,73 +101,73 @@ export default {
     } else if (name === 'report-tahfidz-nilai') {
       dispatch('report/tahfidz/nilai/changeUnit', null, { root: true })
     } else if (name === 'report-pengampu') {
-      if (rootState.index.permissions.includes('report pengampu')) {
+      if (permissions.includes('report pengampu')) {
         dispatch('report/pengampu/changeUnit', null, { root: true })
       } else {
         this.$router.push('/')
       }
     } else if (name === 'report-absensi') {
-      if (rootState.index.permissions.includes('report absensi')) {
+      if (permissions.includes('report absensi')) {
         dispatch('report/absensi/changeUnit', null, { root: true })
       } else {
         this.$router.push('/')
       }
     } else if (name === 'report-jurnal') {
-      if (rootState.index.permissions.includes('report jurnal')) {
+      if (permissions.includes('report jurnal')) {
         dispatch('report/jurnal/changeUnit', null, { root: true })
       } else {
         this.$router.push('/')
       }
     } else if (name === 'settings-pelanggaran') {
-      if (rootState.index.permissions.includes('setup pelanggaran')) {
+      if (permissions.includes('setup pelanggaran')) {
         dispatch('setuppelanggaran/getPelanggaran', state.unit, { root: true })
       } else {
         this.$router.push('/')
       }
     } else if (name === 'santri-kelas') {
-      if (rootState.index.permissions.includes('kelas')) {
+      if (permissions.includes('kelas')) {
         dispatch('santri/kelas/changeUnit', null, { root: true })
       } else {
         this.$router.push('/')
       }
     } else if (name === 'santri-asrama') {
-      if (rootState.index.permissions.includes('asrama')) {
+      if (permissions.includes('asrama')) {
         dispatch('santri/asrama/changeUnit', null, { root: true })
       } else {
         this.$router.push('/')
       }
     } else if (name === 'santri-halaqah') {
-      if (rootState.index.permissions.includes('halaqah')) {
+      if (permissions.includes('halaqah')) {
         dispatch('santri/halaqah/changeUnit', null, { root: true })
       } else {
         this.$router.push('/')
       }
     } else if (name === 'santri-ekskull') {
-      if (rootState.index.permissions.includes('ekskull')) {
+      if (permissions.includes('ekskull')) {
         dispatch('santri/ekskull/changeUnit', null, { root: true })
       } else {
         this.$router.push('/')
       }
     } else if (name === 'pegawai-halaqah') {
-      if (rootState.index.permissions.includes('pengampu')) {
+      if (permissions.includes('pengampu')) {
         dispatch('pegawai/halaqah/changeUnit', state.unit, { root: true })
       } else {
         this.$router.push('/')
       }
     } else if (name === 'pegawai-walas') {
-      if (rootState.index.permissions.includes('wali kelas')) {
+      if (permissions.includes('wali kelas')) {
         dispatch('pegawai/walas/showWalas', state.unit, { root: true })
       } else {
         this.$router.push('/')
       }
     } else if (name === 'pegawai-musyrif') {
-      if (rootState.index.permissions.includes('musyrif')) {
+      if (permissions.includes('musyrif')) {
         dispatch('pegawai/musyrif/showMusyrif', state.unit, { root: true })
       } else {
         this.$router.push('/')
       }
     } else if (name === 'pegawai-mapel') {
-      if (rootState.index.permissions.includes('pengajar')) {
+      if (permissions.includes('pengajar')) {
         dispatch('pegawai/mapel/changeUnit', state.unit, { root: true })
       } else {
         this.$router.push('/')
