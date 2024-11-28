c60420f346ab452c1f140b51292d717fc26c4706
Fauzan Gunawan <fauzan101005gunawan@gmail.com>
2024-11-28 09:45:30 +0700

    report absensi tahfidz problem

--- components/laundry/LaundryInputDataTable.vue
+++ components/laundry/LaundryInputDataTable.vue
@@ -3,12 +3,11 @@
     <div class="mb-3">
       <h2>Data Santri Laundry</h2>
     </div>
-    <div class="table-responsive animate__animated animate__fadeInUp">
+    <div class="table-responsive white-space-nowrap animate__animated animate__fadeInUp">
       <table class="table table-hover table-striped">
         <thead>
           <tr>
             <th scope="col" class="text-start">No</th>
-            <th scope="col" class="nowrap">ID</th>
             <th scope="col" class="nowrap">Receipt</th>
             <th scope="col" class="nowrap">Tanggal</th>
             <th scope="col" class="nowrap">Asrama</th>
@@ -21,7 +20,6 @@
         <tbody>
           <tr v-for="(data, index) in dummyData" :key="index">
             <td class="text-capitalize align-middle nowrap">{{ index + 1 }}</td>
-            <td class="text-capitalize align-middle nowrap">{{ data.ID }}</td>
             <td class="text-capitalize align-middle nowrap">{{ data.Receipt }}</td>
             <td class="text-capitalize align-middle nowrap">{{ data.Tanggal }}</td>
             <td class="text-capitalize align-middle nowrap">{{ data.Asrama }}</td>
@@ -59,8 +57,5 @@ export default {
 input {
   width: 17px;
   height: 17px;
-}
-.nowrap {
-  white-space: nowrap;
 }
 </style>
