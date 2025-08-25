72bd1ac8368533aa2b44e7d3623404d2e11ffac1
haikal <haikal3ahnaf@gmail.com>
2025-08-25 15:57:23 +0700

    rest pin

--- store/qrcode/actions.js
+++ store/qrcode/actions.js
@@ -1,27 +1,27 @@
-import Swal from "sweetalert2"
+import Swal from "sweetalert2";
 export default {
   async changeUnit({ commit, state, dispatch }) {
-    dispatch('index/submitLoad', null, { root: true })
-    const program = localStorage.getItem('program')
-    const reqKelas = this.$apiBase.$get(`get-settings?sk=${program}&type=kelas`)
-    const reqSantri = this.$apiSantri.$get(`get-santri-sisalam?program=${program}&opsi=&method=card&type=`)
-    const [resKelas, resSantri] = await Promise.all([reqKelas, reqSantri])
-    if (resKelas, resSantri) {
-      commit('setState', { key: 'kelas', value: resKelas.kelas })
-      commit('setState', { key: 'santri', value: resSantri })
-      dispatch('index/submitLoad', null, { root: true })
+    dispatch("index/submitLoad", null, { root: true });
+    const program = localStorage.getItem("program");
+    const reqKelas = this.$apiBase.$get(`get-settings?sk=${program}&type=kelas`);
+    const reqSantri = this.$apiSantri.$get(`get-santri-sisalam?program=${program}&opsi=&method=card&type=`);
+    const [resKelas, resSantri] = await Promise.all([reqKelas, reqSantri]);
+    if ((resKelas, resSantri)) {
+      commit("setState", { key: "kelas", value: resKelas.kelas });
+      commit("setState", { key: "santri", value: resSantri });
+      dispatch("index/submitLoad", null, { root: true });
     }
   },
   async getData({ commit, state, dispatch }) {
-    dispatch('index/submitLoad', null, { root: true })
-    const program = localStorage.getItem('program')
-    const kelas = state.selectedKelas
+    dispatch("index/submitLoad", null, { root: true });
+    const program = localStorage.getItem("program");
+    const kelas = state.selectedKelas;
     try {
-      const result = await this.$apiSantri.$get(`get-santri-sisalam?program=${program}&opsi=${kelas}&method=card&type=kelas`)
-      commit('setState', { key: 'santri', value: result })
-      dispatch('index/submitLoad', null, { root: true })
+      const result = await this.$apiSantri.$get(`get-santri-sisalam?program=${program}&opsi=${kelas}&method=card&type=kelas`);
+      commit("setState", { key: "santri", value: result });
+      dispatch("index/submitLoad", null, { root: true });
     } catch (error) {
-      dispatch('index/submitLoad', null, { root: true })
+      dispatch("index/submitLoad", null, { root: true });
       Swal.fire({
         position: "center",
         icon: "error",
@@ -32,46 +32,46 @@ export default {
     }
   },
   async inputCard({ commit, state, dispatch }, event) {
-    commit('BTN')
+    commit("BTN");
     try {
-      const data = Object.fromEntries(new FormData(event.target))
-      const result = await this.$apiCard.$post(`create-card`, data)
+      const data = Object.fromEntries(new FormData(event.target));
+      const result = await this.$apiCard.$post(`create-card`, data);
       if (result) {
-        commit('INPUT_CARD', result)
-        commit('BTN')
+        commit("INPUT_CARD", result);
+        commit("BTN");
       }
     } catch (error) {
-      commit('BTN')
+      commit("BTN");
       Swal.fire({
-        icon: 'error',
+        icon: "error",
         text: error,
         showConfirmButton: false,
-        timer: 1500
-      })
+        timer: 1500,
+      });
     }
   },
   async downloadQr({ commit, state, dispatch }, event) {
-    commit('load')
+    commit("load");
     try {
-      const data = state.selectedCards
-      const file = await this.$apiSantri.$post(`input-santri-sisalam?method=card-xlsx`, data)
+      const data = state.selectedCards;
+      const file = await this.$apiSantri.$post(`input-santri-sisalam?method=card-xlsx`, data);
       if (file) {
-        commit('load')
-        downloadZip(file, `QRCode CNC`)
+        commit("load");
+        downloadZip(file, `QRCode CNC`);
       }
     } catch (error) {
-      commit('load')
+      commit("load");
       Swal.fire({
-        icon: 'error',
+        icon: "error",
         text: error,
         showConfirmButton: false,
-        timer: 1500
-      })
+        timer: 1500,
+      });
     }
   },
   async deleteItem({ commit, state, dispatch }, cnc) {
-    const i = state.santri.findIndex((x) => x.CNC === cnc)
-    const name = state.santri[i].CNC
+    const i = state.santri.findIndex((x) => x.CNC === cnc);
+    const name = state.santri[i].CNC;
     const result = await Swal.fire({
       title: name,
       text: "Kartu akan dihapus!",
@@ -82,22 +82,22 @@ export default {
       confirmButtonText: "Yes, delete it!",
     });
     if (result.isConfirmed) {
-      dispatch('index/submitLoad', null, { root: true })
+      dispatch("index/submitLoad", null, { root: true });
       try {
-        const datas = await this.$apiCard.$delete(`delete-card?sk=${cnc}`)
+        const datas = await this.$apiCard.$delete(`delete-card?sk=${cnc}`);
         if (datas) {
-          commit('DELETE_ITEM', cnc)
+          commit("DELETE_ITEM", cnc);
           Swal.fire({
             position: "center",
             icon: "success",
-            text: 'berhasil dihapus',
+            text: "berhasil dihapus",
             showConfirmButton: false,
             timer: 1500,
           });
-          dispatch('index/submitLoad', null, { root: true })
+          dispatch("index/submitLoad", null, { root: true });
         }
       } catch (error) {
-        dispatch('index/submitLoad', null, { root: true })
+        dispatch("index/submitLoad", null, { root: true });
         Swal.fire({
           position: "center",
           icon: "error",
@@ -108,12 +108,49 @@ export default {
       }
     }
   },
-}
+  async resetPIN({ commit, state, dispatch }, cnc) {
+    const i = state.santri.findIndex((x) => x.CNC === cnc);
+    const name = state.santri[i].CNC;
+    const result = await Swal.fire({
+      title: name,
+      text: "PIN kartu akan direset!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, reset it!",
+    });
+    if (result.isConfirmed) {
+      dispatch("index/submitLoad", null, { root: true });
+      try {
+        const datas = await this.$apiCard.$put(`reset-pin?cnc=${cnc}`);
+        if (datas) {
+          commit("BTN");
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            html: `Nomor PIN baru <span style="font-weight:700">${name}</span> adalah : <span style="font-weight:700">${datas.New_PIN}</span>`,
+            showConfirmButton: true,
+          });
 
+          dispatch("index/submitLoad", null, { root: true });
+        }
+      } catch (error) {
+        dispatch("index/submitLoad", null, { root: true });
+        Swal.fire({
+          position: "center",
+          icon: "error",
+          text: error,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    }
+  },
+};
 
 const downloadZip = (base64String, fileName = "file.zip") => {
-
-  const base64ToBlob = (base64, type = 'application/zip') => {
+  const base64ToBlob = (base64, type = "application/zip") => {
     const byteCharacters = atob(base64);
     const byteNumbers = new Array(byteCharacters.length);
     for (let i = 0; i < byteCharacters.length; i++) {
@@ -125,7 +162,7 @@ const downloadZip = (base64String, fileName = "file.zip") => {
 
   const blob = base64ToBlob(base64String);
 
-  const link = document.createElement('a');
+  const link = document.createElement("a");
   link.href = URL.createObjectURL(blob);
   link.download = fileName;
   document.body.appendChild(link);
