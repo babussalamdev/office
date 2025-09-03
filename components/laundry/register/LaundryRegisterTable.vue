83f3634767db9d20e92064fd44f00bb3151cd53d
haikal <haikal3ahnaf@gmail.com>
2025-09-03 15:55:16 +0700

    reset before monday

--- components/laundry/register/LaundryRegisterTable.vue
+++ components/laundry/register/LaundryRegisterTable.vue
@@ -3,19 +3,18 @@
     <div class="row mb-3">
       <div class="col-12 col-md-6 d-flex flex-column justify-content-center mb-3 mb-md-0">
         <h2 class="mb-3">Tag Register</h2>
-        <select v-model="selectedAsrama" class="form-select" style="font-size: 12px; max-width: max-content;"
-          @change="getDataByAsrama()">
+        <select v-model="selectedAsrama" class="form-select" style="font-size: 12px; max-width: max-content" @change="getDataByAsrama()">
           <option value="" selected disabled>asrama</option>
           <option v-for="(data, index) in listAsrama" :key="index" :value="data.Nama">{{ data.Nama }}</option>
         </select>
       </div>
       <div class="col-12 col-md-6 d-flex justify-content-end align-items-end">
         <div class="input-group">
-          <span style="font-size: 12px !important;" class="bi bi-search input-group-text"></span>
-          <input style="font-size: 12px !important;" type="text" v-model="search" class="form-control"
-            placeholder="Search">
-          <button style="font-size: 12px !important;" class="btn btn-sm btn-primary" data-bs-toggle="modal" data-bs-target="#modalAddDataBag">Input
-            Data</button>
+          <span style="font-size: 12px !important" class="bi bi-search input-group-text"></span>
+          <input style="font-size: 12px !important" type="text" v-model="search" class="form-control" placeholder="Search" />
+          <button style="font-size: 12px !important" class="btn btn-sm btn-primary" data-bs-toggle="modal" data-bs-target="#modalAddDataBag">
+            Input Data
+          </button>
         </div>
       </div>
     </div>
@@ -37,15 +36,28 @@
             <td class="text-capitalize align-middle nowrap">{{ dataItem.Name }}</td>
             <td class="text-capitalize align-middle nowrap">{{ dataItem.Asrama }}</td>
             <td class="text-capitalize align-middle nowrap">{{ dataItem.BagID }}</td>
-            <td class="text-capitalize align-middle nowrap">{{ dataItem.Status }}</td>
+            <td class="text-capitalize align-middle nowrap">
+              <span
+                :class="{
+                  'badge text-bg-success': dataItem.Status === 'active',
+                  'badge text-bg-danger': dataItem.Status === 'lost',
+                  'badge text-bg-warning': dataItem.Status === 'damaged',
+                  'badge text-bg-secondary': dataItem.Status !== 'active',
+                }">
+                {{ dataItem.Status }}
+              </span>
+            </td>
             <td class="text-capitalize align-middle text-end">
               <a href="javascript:;" @click="editItem(dataItem.BagID, dataItem.Status)">
-                <button class="btn btn-sm" :class="{
-                  'bg-primary': dataItem.Status === 'active',
-                  'bg-secondary': dataItem.Status !== 'active',
-                  'border-secondary': !(dataItem.Status === 'active' || dataItem.Status === 'lost')
-                }" :disabled="dataItem.Status === 'lost' || dataItem.Status === 'active' ? false : true">
-                  <i class='bx bx-power-off text-white'></i>
+                <button
+                  class="btn btn-sm"
+                  :class="{
+                    'bg-primary': dataItem.Status === 'active',
+                    'bg-secondary': dataItem.Status !== 'active',
+                    'border-secondary': !(dataItem.Status === 'active' || dataItem.Status === 'lost'),
+                  }"
+                  :disabled="dataItem.Status === 'lost' || dataItem.Status === 'active' ? false : true">
+                  <i class="bx bx-power-off text-white"></i>
                 </button>
               </a>
             </td>
@@ -57,41 +69,41 @@
 </template>
 
 <script>
-import { mapState, mapMutations, mapActions, mapGetters } from 'vuex'
-export default {
-  computed: {
-    ...mapState('laundry/register', ['listAsrama', 'datas']),
-    ...mapGetters('laundry/register', ['getSearch', 'getSelectedAsrama', 'filteredDatas']),
-    search: {
-      get() {
-        return this.getSearch
+  import { mapState, mapMutations, mapActions, mapGetters } from "vuex";
+  export default {
+    computed: {
+      ...mapState("laundry/register", ["listAsrama", "datas"]),
+      ...mapGetters("laundry/register", ["getSearch", "getSelectedAsrama", "filteredDatas"]),
+      search: {
+        get() {
+          return this.getSearch;
+        },
+        set(value) {
+          this.$store.commit("laundry/register/setState", { key: "search", value });
+        },
       },
-      set(value) {
-        this.$store.commit('laundry/register/setState', { key: 'search', value })
-      }
+      selectedAsrama: {
+        get() {
+          return this.getSelectedAsrama;
+        },
+        set(value) {
+          this.$store.commit("laundry/register/setState", { key: "selectedAsrama", value });
+        },
+      },
     },
-    selectedAsrama: {
-      get() {
-        return this.getSelectedAsrama
+    methods: {
+      ...mapActions("laundry/register", ["getDataByAsrama", "editRegisterToActive", "addDataBag"]),
+      ...mapMutations("laundry/register", ["editRegisterLaundry"]),
+      editItem(sk) {
+        const data = this.datas.find((x) => x.BagID === sk);
+        if (data.Status === "lost") {
+          this.editRegisterToActive(sk);
+        } else {
+          this.editRegisterLaundry(sk);
+        }
       },
-      set(value) {
-        this.$store.commit('laundry/register/setState', { key: 'selectedAsrama', value })
-      }
-    }
-  },
-  methods: {
-    ...mapActions('laundry/register', ['getDataByAsrama', 'editRegisterToActive', 'addDataBag']),
-    ...mapMutations('laundry/register', ['editRegisterLaundry']),
-    editItem(sk) {
-      const data = this.datas.find((x) => x.BagID === sk)
-      if ( data.Status === 'lost') {
-        this.editRegisterToActive(sk)
-      } else {
-        this.editRegisterLaundry(sk)
-      }
-    }
-  },
-};
+    },
+  };
 </script>
 
 <style scoped></style>
--- pages/laundry/nota/form/_sk.vue
+++ pages/laundry/nota/form/_sk.vue
@@ -8,20 +8,22 @@
 </template>
 
 <script>
-export default {
-  async asyncData({ store, params, redirect }) {
-    const data = store.state.laundry.nota.datasDetail
-    const sk = params.sk
-    if (data.length === 0) {
-      redirect(`/laundry/nota/${sk.replace('#', '%23')}`)
-    } else {
-      store.commit('laundry/nota/addDataPerSantri')
-    }
-    return { sk }
-  },
-}
+  export default {
+    async asyncData({ store, params, redirect }) {
+      const data = store.state.laundry.nota.datasDetail;
+      console.log(data);
+      const sk = params.sk;
+      if (data.length === 0) {
+        store.commit("laundry/nota/addDataPerSantri");
+        // redirect(`/laundry/nota/${sk.replace('#', '%23')}`)
+      } else {
+        store.commit("laundry/nota/addDataPerSantri");
+      }
+      return { sk };
+    },
+  };
 </script>
 
 <style>
-@import url(~/assets/css/laundry/laundry.css);
+  @import url(~/assets/css/laundry/laundry.css);
 </style>
