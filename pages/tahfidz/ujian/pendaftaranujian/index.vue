7e3143663320533455fd117fdea0beb3047e334f
haikal <haikal3ahnaf@gmail.com>
2026-04-08 15:26:38 +0700

    new ujian menu

---
+++ pages/tahfidz/ujian/pendaftaranujian/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+  export default {
+    async asyncData({ store }) {
+      store.dispatch("rekap/changeUnit");
+    },
+    mounted() {
+      this.closeAllModals();
+    },
+    methods: {
+      closeAllModals() {
+        const backdrop = document.querySelector(".modal-backdrop");
+        if (backdrop) {
+          backdrop.remove();
+        }
+        // Menghapus kelas dan style dari body
+        document.body.classList.remove("modal-open"); // Menghapus kelas
+        document.body.style.overflow = ""; // Menghapus gaya inline
+        document.body.style.paddingRight = ""; // Menghapus gaya inline
+      },
+    },
+  };
+</script>
+
+<style>
+  @import url(~/assets/css/santri/santri.css);
+</style>
+
+<style lang="scss" scoped></style>
--- store/sidebar/mutations.js
+++ store/sidebar/mutations.js
@@ -1,44 +1,46 @@
 export default {
-    notClick(state, list) {
-        state[list] = !state[list]
-    },
-    notClickSub(state, list) {
-      if (state[list + 'SubList'] === true) {
-        state.absensiSubList = false;
-        state.santriSubList = false;
-        state.pegawaiSubList = false;
-        state.databaseSubList = false;
-        state.tahfidzSubList = false;
-        state.tahfidz2SubList = false;
-        state.kelasSubList = false
-        state.settingLaundrySubList = false
-      } else {
-        state.absensiSubList = false;
-        state.santriSubList = false;
-        state.pegawaiSubList = false;
-        state.databaseSubList = false;
-        state.tahfidzSubList = false;
-        state.tahfidz2SubList = false;
-        state.kelasSubList = false
-        state.settingLaundrySubList = false
-        state[list + 'SubList'] = !state[list + 'SubList'];
-      }
-    },
-    // toggleSidebar(state) {
-    //   console.log(state.isSidebar)
-    //   state.isSidebar = state.isSidebar ? false : true;
-    //   console.log(state.isSidebar)
-    // },
-    // tutupSidebar(state) {
-    //   state.isSidebar = true
-    // }
-    TOGGLE_SIDEBAR(state) {
-      state.isSidebarOpen = !state.isSidebarOpen;
-    },
-    CLOSE_SIDEBAR(state) {
-      state.isSidebarOpen = true;
-    },
-    OPEN_SIDEBAR(state) {
-      state.isSidebarOpen = false;
+  notClick(state, list) {
+    state[list] = !state[list];
+  },
+  notClickSub(state, list) {
+    if (state[list + "SubList"] === true) {
+      state.absensiSubList = false;
+      state.santriSubList = false;
+      state.pegawaiSubList = false;
+      state.databaseSubList = false;
+      state.tahfidzSubList = false;
+      state.tahfidz2SubList = false;
+      state.tahfidz3SubList = false;
+      state.kelasSubList = false;
+      state.settingLaundrySubList = false;
+    } else {
+      state.absensiSubList = false;
+      state.santriSubList = false;
+      state.pegawaiSubList = false;
+      state.databaseSubList = false;
+      state.tahfidzSubList = false;
+      state.tahfidz2SubList = false;
+      state.tahfidz3SubList = false;
+      state.kelasSubList = false;
+      state.settingLaundrySubList = false;
+      state[list + "SubList"] = !state[list + "SubList"];
     }
-}
+  },
+  // toggleSidebar(state) {
+  //   console.log(state.isSidebar)
+  //   state.isSidebar = state.isSidebar ? false : true;
+  //   console.log(state.isSidebar)
+  // },
+  // tutupSidebar(state) {
+  //   state.isSidebar = true
+  // }
+  TOGGLE_SIDEBAR(state) {
+    state.isSidebarOpen = !state.isSidebarOpen;
+  },
+  CLOSE_SIDEBAR(state) {
+    state.isSidebarOpen = true;
+  },
+  OPEN_SIDEBAR(state) {
+    state.isSidebarOpen = false;
+  },
+};
